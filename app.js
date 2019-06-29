c41d73594f1ec9ab3799dba475de645ff8e77355
Honey Behal <honey.behal@synapse.com>
2019-06-30 05:28:54 +0530

    added code

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ mongoose.Promise = global.Promise;
 
 //var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
+var bodyParser = require('body-parser');
+
+// support parsing of application/json type post data
+
 
 var app = express();
+var cors = require('cors');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,13 +27,17 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.use('/signUp', usersRouter);
+app.use(bodyParser.json());
 
-
+app.use('/', usersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../schemas/user-registartion-schema');
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /* GET home page. */
 
 router.post('/signUp', function (req, res, next) {
-  console.log("in user router");
-  const test = new User({ email: "testEmail", password: 'ksh' });
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  console.log(req);
+  const test = new User(req.body);
   test.save();
-  debugger;
   // User.create({
   //   email: "email.com",
   //   password: "password"
@@ -16,13 +19,8 @@ router.post('/signUp', function (req, res, next) {
   //   res.send({ hello: 'test' });
   // });
 
-  res.render('index', { title: 'test' });
+  res.send(req.body);
 
-});
-router.get('/', function (req, res, next) {
-  console.log("in user router");
-
-  res.render('index', { title: 'hello2' });
 });
 
 module.exports = router;
